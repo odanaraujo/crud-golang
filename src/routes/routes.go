58de5b3666e5d2920a1c018c12e6118aa08cc355
= <=>
2023-03-21 20:49:03 -0300

    configurando swagger

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -3,9 +3,14 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/odanaraujo/crud-golang/src/controller"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func InitRoutes(r *gin.RouterGroup) {
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 	r.GET("/getUserById/:userId", controller.FindUserByID)
 	r.GET("/user/:userEmail", controller.FindUserByEmail)
 	r.POST("/user", controller.CreateUser)
