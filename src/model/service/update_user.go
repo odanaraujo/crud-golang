153112f9b809aa6dc629068297b9b6f9ccc3bf1c
= <=>
2023-03-20 23:05:38 -0300

    Encriptando senha de user e separando camadas MVC

---
+++ src/model/service/update_user.go
@@ -0,0 +1,10 @@
+package service
+
+import (
+	"github.com/odanaraujo/crud-golang/src/configuration/rest_err"
+	"github.com/odanaraujo/crud-golang/src/model"
+)
+
+func (user *userDomainService) UpdateUser(string, domainInterface model.UserDomainInterface) *rest_err.RestErr {
+	return nil
+}
